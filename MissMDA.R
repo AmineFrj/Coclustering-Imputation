443ba65144ad31909c10694a4622e4e7e29a7c3d
Amine Ferdjaoui <amine.ferdjaoui@orange.com>
2020-04-13 22:55:18 +0200

    CoClustImput

---
+++ MissMDA.R
@@ -0,0 +1,53 @@
+library(rmatio)
+library(missMDA)
+library(mice)
+
+webACE <- read.mat('datasets/WebACE.mat')
+#webACE$fea
+amputeWebACE <- ampute(webACE$fea, 0.3)
+
+# Data : Données
+# tauxMissing : % de données missing (valeurs entre 0 et 1 avec 0.5 = 50%)
+genMissingData <- function(datas, tauxMissing = 0.3) {
+  print("just check if 2 next numbers are equals...")
+  dimx = dim(datas)[1]
+  dimy = dim(datas)[2]
+  samples <- as.integer(length(datas) * tauxMissing)
+  print(samples)
+  vals <- sample.int(dimx*dimy, samples, replace = F)
+  
+  x = cbind(vals %% dimx + 1, vals %% dimy + 1)
+  #dim(unique(x))
+  datas[x] = NA
+  print(sum(is.na(datas)))
+  return(datas)
+  
+}
+
+
+missWebACE <- genMissingData(webACE$fea, 0.2)
+sum(is.na(missWebACE))
+NAs <- which(is.na(missWebACE), arr.ind=TRUE)
+
+res <- imputeCA(missWebACE)
+which(is.na(amputeWebACE$amp), arr.ind=TRUE)
+
+write.mat(as.data.frame(NAs),'NaNs.mat')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ coclust/README.txt
@@ -0,0 +1,17 @@
+Installation
+============
+
+The package relies on the NumPy, SciPy and scikit-learn libraries, and also on
+Matplotlib for some of the demos included in the package.
+
+If these libraries are already installed on your machine, you can install
+coclust from PyPI by just entering:
+
+    pip install coclust
+
+See https://pypi.python.org/pypi/coclust and http://coclust.readthedocs.org for
+additional installation and usage notes.
+
+The package can also be installed from the provided source code by running:
+
+    python setup.py install
